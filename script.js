028b49741b856c26e9ace19b6ef58277e2a857c5
Alicite <lunacarter1503@gmail.com>
2026-06-03 21:32:48 -0300

    aula 03/06

--- script.js
+++ script.js
@@ -19,12 +19,13 @@ const enviarAluno = async () => {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({user: { nome, idade, idiomas: ["pt-BR"] }})
+            body: JSON.stringify({user: { nome, idade }})
         }
         let resposta = await fetch('http://localhost:3000/users', options);
         resposta = await resposta.json();
         feedback.textContent = resposta;
-
+        limparAlunos();
+        await renderAlunos();
     } catch (e) {
         console.error(e);
     }
@@ -42,56 +43,83 @@ const removerAluno = async (event) => {
     }
 };
 
-const main = async () => {
+const renderAlunos = async () => {
     const alunos = await fetchGETAlunos();
     const elemUl = document.querySelector('#lista-alunos');
 
     for (let aluno of alunos) {
-        console.log(aluno);
         const elemLi = document.createElement('li');
 
-        const idiomas = aluno.idiomas ? aluno.idiomas : 'Não foram encontrados idiomas';
-
-        elemLi.innerHTML = `Nome: ${aluno.nome} - Idade: ${aluno.idade} - Idiomas: ${idiomas}`;
+        elemLi.innerHTML = `Nome: ${aluno.nome} - Idade: ${aluno.idade}`;
         const btRemover = document.createElement('button');
         btRemover.dataset.id = aluno._id;
         btRemover.textContent = '❌';
         btRemover.onclick = removerAluno;
 
+        const btEditar = document.createElement('button');
+        btEditar.dataset.id = aluno._id;
+        btEditar.textContent = '✏️';
+        btEditar.onclick = editarAluno;
+
         elemLi.appendChild(btRemover);
+        elemLi.appendChild(btEditar);
         elemUl.appendChild(elemLi);
+    }
+};
+
+const editarAluno = async (evento) => {
+    const inputNome = document.createElement('input');
+    inputNome.type = 'text';
+    inputNome.placeholder = 'Nome';
+    inputNome.id = 'editar-nome';
+
+    const inputIdade = document.createElement('input');
+    inputIdade.type = 'number';
+    inputIdade.placeholder = 'Idade';
+    inputIdade.id = 'editar-idade';
+
+    const btAtualizar = document.createElement('button');
+    btAtualizar.innerText = '✔️';
+    btAtualizar.dataset.id = evento.target.dataset.id;
+    btAtualizar.onclick = atualizarAluno;
+
+    evento.target.parentNode.appendChild(inputNome);
+    evento.target.parentNode.appendChild(inputIdade);
+    evento.target.parentNode.appendChild(btAtualizar);
+}
+
+const atualizarAluno = async (evento) => {
+    const nome = document.querySelector('#editar-nome').value;
+    const idade = document.querySelector('#editar-idade').value;
+    const feedback = document.querySelector('#feedback');
+
+    try {
+        const options = {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({user: { nome, idade}})
+        }
+        let resposta = await fetch(`http://localhost:3000/users/${evento.target.dataset.id}`, options);
+        resposta = await resposta.json();
+        feedback.textContent = resposta;
+        limparAlunos();
+        await renderAlunos();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+const limparAlunos = () => {
+    const elemUl = document.querySelector('#lista-alunos');
+
+    while (elemUl.firstChild) {
+        elemUl.removeChild(elemUl.firstChild);
     }
 }
 
 const botaoEnviar = document.querySelector('#enviar');
-main()
+renderAlunos()
 
 botaoEnviar.addEventListener('click', enviarAluno);
-// botaoEnviar.addEventListener('click', async () => {
-//     const nome = document.querySelector("#novo-nome").value;
-//     const idade = document.querySelector("#novo-idade").value;
-//     const feedback = document.querySelector('#feedback');
-
-//     const body = { user: { nome, idade } };
-    
-//     try {
-//         const options = {
-//             method: 'POST',
-//             body: Object.keys(body)
-//             .map(k => `${encodeURIComponent(k)}=${encodeURIComponent(body[k])}`)
-//             .join('&'),
-//             headers: {
-//                 "Access-Control-Allow-Origin": "http://localhost:3000/"
-//             }
-//         }
-//         console.log(options);
-//         let resposta = await fetch('http://localhost:3000/users', options);
-//         resposta = await resposta.json();
-//         feedback.innerHTML = resposta;
-//         console.log(resposta);
-
-
-//     } catch (e) {
-//         console.error(e);
-//     }
-// });
